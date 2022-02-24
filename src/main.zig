b1143583a42f12b15ff82ec824562184d5a9c435
Dante Catalfamo <43040593+dantecatalfamo@users.noreply.github.com>
2022-02-23 21:27:09 -0500

    Show copyright in main

--- src/main.zig
+++ src/main.zig
@@ -2,12 +2,13 @@ const std = @import("std");
 const mruby = @import("mruby.zig");
 
 pub fn main() anyerror!void {
-    var mrb = mruby.mrb_open();
+    var mrb = mruby.mrb_open() orelse return error.OpenError;
+    defer mruby.mrb_close(mrb);
     std.log.debug("state pointer: {p}", .{ mrb });
-    std.log.info("All your codebase are belong to us.", .{});
-    _ = mruby.mrb_value;
+    mruby.mrb_show_version(mrb);
+    mruby.mrb_show_copyright(mrb);
 }
 
-test "basic test" {
-    try std.testing.expectEqual(10, 3 + 7);
+test "ref all decls" {
+    std.testing.refAllDecls(mruby);
 }
