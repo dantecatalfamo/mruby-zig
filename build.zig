214fa353d648e82441bd9b13bc73aa5f9d66ab95
Dante Catalfamo <43040593+dantecatalfamo@users.noreply.github.com>
2022-03-04 17:03:31 -0500

    Fix build on MacOS 12.1, add steps to pull in and build mruby

--- build.zig
+++ build.zig
@@ -20,9 +20,26 @@ pub fn build(b: *std.build.Builder) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
+    const submodule_step = b.step("submodule", "Pull in git submodule");
+    submodule_step.makeFn = fetchSubmodule;
+
+    const mruby_step = b.step("mruby", "Build mruby");
+    mruby_step.makeFn = buildMruby;
+    mruby_step.dependOn(submodule_step);
+
     const exe = b.addExecutable("mruby-zig", "src/main.zig");
+    exe.step.dependOn(mruby_step);
     exe.setTarget(target);
     exe.setBuildMode(mode);
+    // Won't compile on my macbook without this
+    // zig version 0.10.0-dev.934+acec06cfa
+    // fails with the following error:
+    //     error(compilation): clang failed with stderr: In file included from /Users/dante/src/github.com/dantecatalfamo/mruby-zig/src/mruby_compat.c:4:
+    //     In file included from /Users/dante/src/github.com/dantecatalfamo/mruby-zig/mruby/include/mruby.h:117:
+    //     /Users/dante/src/github.com/dantecatalfamo/mruby-zig/mruby/include/mruby/value.h:426:10: fatal error: 'mach-o/getsect.h' file not found
+    if (target.isDarwin()) {
+        exe.addFrameworkPath("/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX12.1.sdk");
+    }
     exe.addSystemIncludePath("./mruby/include");
     exe.addLibraryPath("./mruby/build/host/lib");
     exe.linkSystemLibrary("mruby");
@@ -42,6 +59,9 @@ pub fn build(b: *std.build.Builder) void {
     const exe_tests = b.addTest("src/main.zig");
     exe_tests.setTarget(target);
     exe_tests.setBuildMode(mode);
+    if (target.isDarwin()) {
+        exe.addFrameworkPath("/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX12.1.sdk");
+    }
     exe_tests.addSystemIncludePath("./mruby/include");
     exe_tests.addLibraryPath("./mruby/build/host/lib");
     exe_tests.linkSystemLibrary("mruby");
@@ -50,4 +70,22 @@ pub fn build(b: *std.build.Builder) void {
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&exe_tests.step);
+}
+
+pub fn buildMruby(self: *std.build.Step) !void {
+    _ = self;
+    var allocator = std.heap.page_allocator;
+    try std.os.chdir("mruby");
+    var process = try std.ChildProcess.init(&.{"rake"}, allocator);
+    defer process.deinit();
+    _ = try process.spawnAndWait();
+    try std.os.chdir("..");
+}
+
+pub fn fetchSubmodule(self: *std.build.Step) !void {
+    _ = self;
+    var allocator = std.heap.page_allocator;
+    var process = try std.ChildProcess.init(&.{"git", "submodule", "update", "--init"}, allocator);
+    defer process.deinit();
+    _ = try process.spawnAndWait();
 }
