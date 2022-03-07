25516b734688da669db45b52fddb904afb6f7cdc
dantecatalfamo <dante.catalfamo@gmail.com>
2022-03-07 00:00:14 -0500

    Fix tests in build.zig

--- build.zig
+++ build.zig
@@ -43,7 +43,7 @@ pub fn build(b: *std.build.Builder) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const exe_tests = b.addTest("src/main.zig");
+    const exe_tests = b.addTest("examples/main.zig");
     exe_tests.setTarget(target);
     exe_tests.setBuildMode(mode);
     addMruby(exe_tests);
